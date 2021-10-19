4191f5e8a663526a976392c45bb0efe99c331d11
chowdhuryali <chowdhuryali1400@gmail.com>
2021-10-19 11:26:54 -0400

    Duplicate Word

--- src/problems/DuplicateWord.java
+++ src/problems/DuplicateWord.java
@@ -24,10 +24,10 @@ public class DuplicateWord {
         System.out.println("Duplicate words in given string:");
         for (int i = 0; i < words.length; i++) {
             count = 1;
-            for (int j = i + 1; j < words.length; j++) {
-                if (words[i].equals(words[j])) {
+            for (int k = i + 1; k < words.length; k++) {
+                if (words[i].equals(words[k])) {
                     count++;
-                    words[j] = "0";
+                    words[k] = "0";
 
                 }
 
